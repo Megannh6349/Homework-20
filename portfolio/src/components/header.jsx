a4a018b7bda036464154b4be2923f9294ad590c2
Megan H <megannh6349@gmail.com>
2020-08-05 01:10:55 -0400

    troubleshooting href issue

--- portfolio/src/components/header.jsx
+++ portfolio/src/components/header.jsx
@@ -1,58 +1,43 @@
-import React from 'react';
-
-var header = React.createClass({
-    render: function () {
+import React, { Component } from 'react';
+export default class Header extends Component {
+    render() {
         return (
-            <div>
-                import React, {'{'} Component {'}'} from 'react';
-        export default class Header extends Component {'{'}
-        render() {'{'}
-        return (
-                <react.fragment>
-                    <header id="home">
-                        <nav id="nav-wrap">
-                            <a className="mobile-btn" href="#nav-wrap" title="Show navigation">Show navigation</a>
-                            <a className="mobile-btn" href="#" title="Hide navigation">Hide navigation</a>
-                            <ul id="nav" className="nav">
-                                <li className="current"><a className="smoothscroll" href="#home">Home</a></li>
-                                <li><a className="smoothscroll" href="#about">About</a></li>
-                                <li><a className="smoothscroll" href="#resume">Resume</a></li>
-                                <li><a className="smoothscroll" href="#portfolio">Works</a></li>
-                                <li><a className="smoothscroll" href="#testimonials">Testimonials</a></li>
-                                <li><a className="smoothscroll" href="#contact">Contact</a></li>
-                            </ul> {/* end #nav */}
-                        </nav> {/* end #nav-wrap */}
-                        <div className="row banner">
-                            <div className="banner-text">
-                                <h1 className="responsive-headline">I'm Jonathan Doe.</h1>
-                                <h3>I'm a Manila based <span>graphic designer</span>, <span>illustrator</span> and <span>webdesigner</span>
-                    creating awesome and
-                    effective visual identities for companies of all sizes around the globe. Let's <a className="smoothscroll" href="#about">start scrolling</a>
-                    and learn more <a className="smoothscroll" href="#about">about me</a>.</h3>
-                                <hr />
-                                <ul className="social">
-                                    <li><a href="#"><i className="fa fa-facebook" /></a></li>
-                                    <li><a href="#"><i className="fa fa-twitter" /></a></li>
-                                    <li><a href="#"><i className="fa fa-google-plus" /></a></li>
-                                    <li><a href="#"><i className="fa fa-linkedin" /></a></li>
-                                    <li><a href="#"><i className="fa fa-instagram" /></a></li>
-                                    <li><a href="#"><i className="fa fa-dribbble" /></a></li>
-                                    <li><a href="#"><i className="fa fa-skype" /></a></li>
-                                </ul>
-                            </div>
-                        </div>
-                        <p className="scrolldown">
-                            <a className="smoothscroll" href="#about"><i className="icon-down-circle" /></a>
-                        </p>
-                        <header id="home">
-                            ....
+            <header id="home">
+                <nav id="nav-wrap">
+                    <a className="mobile-btn" href="#nav-wrap" title="Show navigation">Show navigation</a>
+                    <a className="mobile-btn" href="https://www.google.com" title="Hide navigation">Hide navigation</a>
+                    <ul id="nav" className="nav">
+                        <li className="current"><a className="smoothscroll" href="#home">Home</a></li>
+                        <li><a className="smoothscroll" href="#about">About</a></li>
+                        <li><a className="smoothscroll" href="#resume">Resume</a></li>
+                        <li><a className="smoothscroll" href="#portfolio">Works</a></li>
+                        <li><a className="smoothscroll" href="#testimonials">Testimonials</a></li>
+                        <li><a className="smoothscroll" href="#contact">Contact</a></li>
+                    </ul> {/* end #nav */}
+                </nav> {/* end #nav-wrap */}
+                <div className="row banner">
+                    <div className="banner-text">
+                        <h1 className="responsive-headline">I'm Jonathan Doe.</h1>
+                        <h3>I'm a Manila based <span>graphic designer</span>, <span>illustrator</span> and <span>webdesigner</span>
+            creating awesome and
+            effective visual identities for companies of all sizes around the globe. Let's <a className="smoothscroll" href="#about">start scrolling</a>
+            and learn more <a className="smoothscroll" href="#about">about me</a>.</h3>
+                        <hr />
+                        <ul className="social">
+                            <li><a href="https://www.google.com"><i className="fa fa-facebook" /></a></li>
+                            <li><a href="https://www.google.com"><i className="fa fa-twitter" /></a></li>
+                            <li><a href="https://www.google.com"><i className="fa fa-google-plus" /></a></li>
+                            <li><a href="https://www.google.com"><i className="fa fa-linkedin" /></a></li>
+                            <li><a href="https://www.google.com"><i className="fa fa-instagram" /></a></li>
+                            <li><a href="https://www.google.com"><i className="fa fa-dribbble" /></a></li>
+                            <li><a href="https://www.google.com"><i className="fa fa-skype" /></a></li>
+                        </ul>
+                    </div>
+                </div>
+                <p className="scrolldown">
+                    <a className="smoothscroll" href="#about"><i className="icon-down-circle" /></a>
+                </p>
             </header>
-            );
-            {'}'}
-                        {'}'}
-                    </header></react.fragment></div>
         );
     }
-});
-
-export default header;
+}
