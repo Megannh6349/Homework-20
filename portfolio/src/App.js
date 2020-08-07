d0ca2c33350126f027dbc92dbf407b1d2fe4019a
Megan H <megannh6349@gmail.com>
2020-08-07 01:37:53 -0400

    materialize updates

--- portfolio/src/App.js
+++ portfolio/src/App.js
@@ -1,36 +1,20 @@
 import React, { Component } from 'react';
 import './App.css';
 import 'materialize-css';
-import { Parallax } from 'react-materialize';
+import { Row, Col } from 'react-materialize';
 
 
 export default class App extends Component {
   render() {
     return (
-<div>
-  <Parallax
-    image={<img alt="" src="http://materializecss.com/images/parallax1.jpg"/>}
-    options={{
-      responsiveThreshold: 0
-    }}
-  />
-  <div className="section white">
-    <div className="row container">
-      <h2 className="header">
-        Parallax
-      </h2>
-      <p className="grey-text text-darken-3 lighten-3">
-        Parallax is an effect where the background content or image in this case, is moved at a different speed than the foreground content while scrolling.
-      </p>
-    </div>
-  </div>
-  <Parallax
-    image={<img alt="" src="http://materializecss.com/images/parallax2.jpg"/>}
-    options={{
-      responsiveThreshold: 0
-    }}
-  />
-</div>
+      <div>
+        <Row>
+          <Col className="sidebar"
+          s={1}>sidebar</Col>
+          <Col className="mainLanding"
+          s={1}>mainLanding</Col>
+        </Row>
+      </div>
     )
   }
 };
