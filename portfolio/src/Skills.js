aa9a7548e962c1cc02fed91dadb76911339a5e81
Megan H <megannh6349@gmail.com>
2020-08-12 11:18:39 -0400

    created Skills page

--- portfolio/src/Skills.js
+++ portfolio/src/Skills.js
@@ -1,8 +1,9 @@
 import React from 'react';
 import './App.css';
 import NavbarComp from './components/navbar.js';
-
+import SkillsPage from './components/skills.js';
 import FooterComp from './components/footer.js';
+
 
 
 class Skills extends React.Component {
@@ -10,7 +11,7 @@ class Skills extends React.Component {
         return (
             <div>
                 <div id="Home"><NavbarComp /></div>
-
+                <SkillsPage />
                 <div><FooterComp /></div>
             </div>
         )
